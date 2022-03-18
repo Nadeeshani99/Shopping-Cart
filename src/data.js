34e63fc5581b3dc9cad11232de88a9b3f0570561
Nadeeshani99 <anuradanadishani6@gmail.com>
2022-03-19 03:38:13 +0530

    Create Repository

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Dragon Fruit',
+      price: 1.5,
+      image: 'https://th.bing.com/th?id=OIF.qmLGEWhI8HpiOp7%2bvsyTFg&w=249&h=180&c=7&r=0&o=5&pid=1.7',
+    },
+    {
+      id: '2',
+      name: 'Pomegranete Fruit',
+      price: 0.75,
+      image: 'https://th.bing.com/th/id/OIP.C2yRvmtMBxguk0l6W6bTTgHaGI?pid=ImgDet&rs=1',
+    },
+    {
+      id: '3',
+      name: 'Pineapple',
+      price: 0.5,
+      image: 'https://th.bing.com/th/id/OIP.rDJ5jipDansl_ctRpBzGZQHaHa?w=195&h=195&c=7&r=0&o=5&pid=1.7',
+    },
+    {
+      id: '4',
+      name: 'Pear Fruit',
+      price: 0.5,
+      image: 'https://th.bing.com/th/id/OIP.FvekmxfgkUj6eAWBkD-K4gHaFj?w=243&h=182&c=7&r=0&o=5&pid=1.7',
+    },
+  ],
+};
+export default data;
